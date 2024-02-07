ab16b563b16db0c2da61d4ffce946ce682332219
Eduardo Villa <151227243+evilla092@users.noreply.github.com>
2024-02-07 20:03:51 +0000

    change

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,18 @@
 Rails.application.routes.draw do
+  get "/", controller: "calc", action: "square_ask"
+  get "/square" controller: "calc", action: "square_ask"
 
-get "/", controller: "calc", action: "square_ask"
+  # get "/square/result" controller: "calc", action: "square_result"
 
-get "/square" controller: "calc", action: "square_ask"
+  # get "/square_root_ask" controller: "calc", action: "square_root_ask"
 
-get "/square/result" controller: "calc", action: "square_result"
+  # get "/square_root_ask/result" controller: "calc", action: "square_root_result"
 
-get "/square_root" controller: "calc", action: "square_root_ask"
+  # get "/payment_ask" controller: "calc", action: "payment_ask"
 
-get "/square_root/result" controller: "calc", action: "square_root_result"
+  # get "/payment/result" controller: "calc", action: "payment_result"
 
-get "/payment" controller: "calc", action: "payment_ask"
+  # get "/random_ask" controller: "calc", action: "random_ask"
 
-get "/payment/result" controller: "calc", action: "payment_result"
-
-get "/random" controller: "calc", action: "random_ask"
-
-get "/random/result" controller: "calc", action: "random_result"
-  
+  # get "/random/result" controller: "calc", action: "random_result"
 end
